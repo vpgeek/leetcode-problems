374d6c5b31e4717a1a6763eaac91565f462e2a11
VP <p.valli.se@gmail.com>
2023-05-15 01:27:20 +0530

    875 - koko eating bananas

---
+++ PHP/Binary-Search/875_koko_eating_bananas.php
@@ -0,0 +1,73 @@
+<?php
+/*
+875. Koko Eating Bananas - Medium
+
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all the bananas within h hours.
+
+Example 1:
+Input: piles = [3,6,7,11], h = 8
+Output: 4
+
+Example 2:
+Input: piles = [30,11,23,4,20], h = 5
+Output: 30
+
+Example 3:
+Input: piles = [30,11,23,4,20], h = 6
+Output: 23
+ 
+Constraints:
+
+1 <= piles.length <= 104
+piles.length <= h <= 109
+1 <= piles[i] <= 109
+*/
+
+/*
+Big O Analysis:
+Time Complexity - O(n log max(p))
+Space Compleity - O(1)
+
+*/
+
+class Solution {
+
+    /**
+     * @param Integer[] $piles
+     * @param Integer $h
+     * @return Integer
+     */
+    function minEatingSpeed($piles, $h) {
+        // set k to max possible no
+        $k = max($piles);
+        // $k range should be [1, 2, 3, ..., max[$piles]]
+        $l = 1;
+        $r = max($piles);
+        while ($l <= $r) {
+            $mid = floor(($l + $r) / 2);
+            $hours = 0;
+            for ($i=0; $i<count($piles); $i++) {
+                // count no of hours to eat each pile with eating speed as mid val
+                $hours += ceil($piles[$i] / $mid);
+            }
+            if ($hours <= $h) {
+                // decrease eating speed
+                $k = min($k, $mid);
+                $r = $mid - 1;
+            } else {
+                // increase eating speed
+                $l = $mid + 1;
+            }
+        }
+        return $k;
+    }
+}
+
+$s = new Solution();
+echo $s->minEatingSpeed([30,11,23,4,20], 5);
