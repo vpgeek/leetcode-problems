41934f4208da5a913c69b10716f5691943c9e633
VP <p.valli.se@gmail.com>
2023-05-23 02:25:49 +0530

    minor changes

--- Java/Binary-Search/LP-875-Koko-Eating-Bananas.java
+++ Java/Binary-Search/LP-875-Koko-Eating-Bananas.java
@@ -36,9 +36,8 @@ Space Compleity - O(1)
 */
 
 import java.util.Arrays;
-import java.util.Collections;
 
-public class Solution {
+class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
