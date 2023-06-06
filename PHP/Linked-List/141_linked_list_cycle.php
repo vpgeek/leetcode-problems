f1933142746a4fbfffd667758383649cacdf12c4
VP <p.valli.se@gmail.com>
2023-06-06 16:19:22 +0530

    minor changes

--- PHP/Linked-List/141_linked_list_cycle.php
+++ PHP/Linked-List/141_linked_list_cycle.php
@@ -64,7 +64,7 @@ class Solution {
         while ($fast != null && $fast->next != null) {
             $slow = $slow->next;
             $fast = $fast->next->next;
-            if ($slow == $fast) {
+            if ($slow === $fast) {
                 return true;
             }
         }
