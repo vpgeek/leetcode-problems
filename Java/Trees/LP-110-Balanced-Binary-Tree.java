1e903c694dddb34f5109341ae41e923d1e2b01c8
VP <p.valli.se@gmail.com>
2023-06-14 03:48:59 +0530

    java - 110 - balance binary tree

---
+++ Java/Trees/LP-110-Balanced-Binary-Tree.java
@@ -0,0 +1,81 @@
+/*
+110. Balanced Binary Tree - Easy
+
+Given a binary tree, determine if it is height-balanced
+
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+
+Example 2:
+
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+
+Example 3:
+
+Input: root = []
+Output: true
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-104 <= Node.val <= 104
+
+*/
+
+/**
+ * Big O Analysis
+ * 
+ * Time Complexity - O(n)
+ * Space Complexity - O(n) 
+ */
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (maxDepth(root) == -1) return false;
+        return true;
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        if (leftDepth == -1 || rightDepth == -1 || Math.abs(leftDepth - rightDepth) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        Solution s = new Solution();
+        System.out.println(s.isBalanced(root));
+    }
+
+}
+
