05cb9b8b913751d3d9743e83a37840d9275bef01
VP <p.valli.se@gmail.com>
2023-08-06 03:02:40 +0530

    746 - min cost climbing stairs

---
+++ Java/1-D-Dynamic-Programming/LP-746-Min-Cost-Climbing-Stairs.java
@@ -0,0 +1,88 @@
+/*
+746. Min Cost Climbing Stairs - Easy
+
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+
+Example 1:
+
+Input: cost = [10,15,20]
+Output: 15
+Explanation: You will start at index 1.
+- Pay 15 and climb two steps to reach the top.
+The total cost is 15.
+
+Example 2:
+
+Input: cost = [1,100,1,1,1,100,1,1,100,1]
+Output: 6
+Explanation: You will start at index 0.
+- Pay 1 and climb two steps to reach index 2.
+- Pay 1 and climb two steps to reach index 4.
+- Pay 1 and climb two steps to reach index 6.
+- Pay 1 and climb one step to reach index 7.
+- Pay 1 and climb two steps to reach index 9.
+- Pay 1 and climb one step to reach the top.
+The total cost is 6.
+
+Constraints:
+
+2 <= cost.length <= 1000
+0 <= cost[i] <= 999
+
+*/
+
+/*
+ * Big O Analysis
+ * 
+ * Time Complexity - O(n)
+ * Space Complexity - O(1)
+ */
+
+class Solution {
+    public static int minCostClimbingStairsDp(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n+1];
+        for (int i=0; i<=n; i++) {
+            if (i == 0 || i == 1) {
+                dp[i] = cost[i];
+            } else {
+                dp[i] = Math.min(dp[i-1], dp[i-2]) + (i < n ? cost[i] : 0);
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int first = cost[0];
+        int second = cost[1];
+
+        if (n == 1 ) {
+            return first;
+        }
+
+        if (n == 2) {
+            return Math.min(first, second);
+        }
+
+        int minCost = 0;
+        for (int i=2; i<=n; i++) {
+            minCost = Math.min(first, second) + (i < n ? cost[i] : 0);
+            first = second;
+            second = minCost;
+        }
+
+        return minCost;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(minCostClimbingStairs(new int[]{1,100,1,1,1,100,1,1,100,1}));
+        System.out.println(minCostClimbingStairs(new int[]{10,15,20}));
+    }
+}
