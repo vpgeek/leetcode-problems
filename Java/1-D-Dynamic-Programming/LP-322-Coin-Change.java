f026b69365b50f976fa474cd9889251a35f37ef5
VP <p.valli.se@gmail.com>
2023-08-21 03:25:44 +0530

    322 - coin change changes

--- Java/1-D-Dynamic-Programming/LP-322-Coin-Change.java
+++ Java/1-D-Dynamic-Programming/LP-322-Coin-Change.java
@@ -31,17 +31,32 @@ Constraints:
 
 */
 
-/*
-Big O Analysis:
-Time Complexity - O(amount * n)
-Space Complexity - O(amount)
-
-*/
+/**
+ * Big O Analysis:
+ * Time Complexity - O(amount * n)
+ * Space Complexity - O(amount)
+ * 
+ */
 
 import java.util.Arrays;
 
 class Solution {
     public static int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int i=coin; i<=amount; i++) {
+                if (dp[i-coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i-coin]+1);
+                }
+            }
+        }
+
+        return (dp[amount] == Integer.MAX_VALUE) ? -1 : dp[amount];
+    }
+
+    public static int coinChangeOld(int[] coins, int amount) {
         int[] dp = new int[amount+1];
         Arrays.fill(dp, amount+1);
         dp[0] = 0;
